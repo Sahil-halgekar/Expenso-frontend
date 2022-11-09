70e7c5caa31e61158ccdcfd248af48c86e3a6b39
Sahil Halgekar <sahilhalgekar07@gmail.com>
2022-11-09 20:22:42 +0530

    changes to build

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,4 +1,4 @@
-const API_URL = "https://expenso-backend.vercel.app/api/" || 'http://localhost:5000/api';
+const API_URL = "https://expenso-backend.vercel.app/api" || 'http://localhost:5000/api';
 
 const CURRENCIES = [
   'AFA',
